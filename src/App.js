4169cee8c3bdaeef9ad0b9d76787fdbd8bf24dd7
Sourav Singh <souravsingh2609@gmail.com>
2024-03-22 12:43:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs.jsx';
+
+
+
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+          <Programs/>
+    </div>
+  );
+}
+
+export default App;
