0a3edba55d6b677bd55775cc0b024260eeae4e60
Sourav Singh <souravsingh2609@gmail.com>
2024-03-27 08:57:25 +0530

    Testimonials

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join.jsx';
 import Plans from './components/Plans/Plans.jsx';
 import Programs from './components/Programs/Programs.jsx';
 import Reasons from './components/Reasons/Reasons.jsx';
+import Testimonials from './components/Testimonials/Testimonials.jsx';
+import Footer from './components/Footer/Footer.jsx'
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
           <Programs/>
           <Reasons />
           <Plans />
+          <Testimonials />
+          <Join />
+          <Footer />
     </div>
   );
 }
