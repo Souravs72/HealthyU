f3e3ede2f1ad87bdbccff688d916310430ae8c96
Sourav Singh <souravsingh2609@gmail.com>
2024-03-28 15:44:37 +0530

    Animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,10 +6,13 @@ import Heart from '../../assets/heart.png';
 import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Calories from "../../assets/calories.png";
-
+import {motion} from 'framer-motion'
 
 
 const Hero = () => {
+
+    const transition = {type: 'just', duration : 3}
+
     return (
         <div className="hero">
 
@@ -21,7 +24,12 @@ const Hero = () => {
                 <Header />
                 {/* the best ad */}
                 <div className="the-best-ad">
-                    <div></div>
+                    <motion.div
+                        initial={{left: '340px'}}
+                        whileInView={{left: '8px'}}
+                        transition={{...transition, type: 'tween'}}
+                    >
+                    </motion.div>
                     <span>the best fitness club in the country</span>
                 </div>
 
@@ -67,14 +75,23 @@ const Hero = () => {
 
             <div className="right-h">
                 <button className="btn">Join Now</button>
-                <div className="heart-rate">
+                <motion.div
+                    initial={{right: "-1rem"}}
+                    whileInView={{right: "4rem"}}
+                    transition={{...transition, type: 'just'}}
+                    className="heart-rate">
+
                     <img src={Heart} alt="" />
                     <span>Heart Rate </span>
                     <span>116 BPM</span>
-                </div>
+                </motion.div>
 
                 <img src={hero_image} alt="" className='hero-image' />
-                <img src={hero_image_back} alt="" className='hero-image-back'/>
+                <motion.img 
+                initial={{right: '11rem'}}
+                whileInView={{right: '20rem'}}
+                transition={transition}
+                src={hero_image_back} alt="" className='hero-image-back'/>
 
                 {/* Calories */}
                 <div className="calories">
